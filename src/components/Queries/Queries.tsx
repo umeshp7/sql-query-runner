3d6edbeb1723f7d483e31e4f12f0d7027bfbe88c
Umesh Pathak <31915995+umeshp7@users.noreply.github.com>
2025-03-11 18:54:21 +0530

    Additional functionalities (#3)
    * add a download csv button and sorting of table
    
    * add the queries to local storage
    
    * added a way to filter through the queries
    
    ---------
    
    Co-authored-by: Umesh Pathak <umesh@Umeshs-MacBook-Air.local>

--- src/components/Queries/Queries.tsx
+++ src/components/Queries/Queries.tsx
@@ -1,14 +1,14 @@
-import React, { JSX, memo } from 'react';
+import React, { JSX, memo, useState } from 'react';
 import './Queries.css';
 
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import List from '@mui/material/List';
-
+import TextField from '@mui/material/TextField';
+import ListItem from '@mui/material/ListItem';
 
 import CodeMirror, { EditorView } from '@uiw/react-codemirror';
 import { sql } from '@codemirror/lang-sql';
-import { ListItem } from '@mui/material';
 
 type QueriesProps = {
   selectQuery?: (query: string) => void;
@@ -16,16 +16,18 @@ type QueriesProps = {
   history: string[];
 };
 
+function Queries({ selectQuery, queries, history }: QueriesProps): JSX.Element {
+  const [activeTab, setActiveTab] = useState('save');
+  const [searchTerm, setSearchTerm] = useState('');
 
-function Queries ({
-  selectQuery,
-  queries,
-  history
-} : QueriesProps) : JSX.Element {
-  const [activeTab, setActiveTab] = React.useState('save');
   const handleSelect = (query: string) => {
     selectQuery && selectQuery(query || '');
   };
+
+  const filteredQueries =
+  (activeTab === 'save' ? queries : history).filter(query =>
+    query.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div className='queries'>
@@ -37,54 +39,35 @@ function Queries ({
         <Tab label='Saved' value='save' />
         <Tab label='History' value='history' />
       </Tabs>
-      {/* @todo reorganize */}
-      {activeTab === 'save' ? (
-        <div className='queries-list'>
-          <List>
-            {queries.map((query, index) => (
-              <ListItem
-                key={index}
-                onClick={() => handleSelect(query)}
-              >
-                <CodeMirror
-                  value={query}
-                  style={{ cursor: 'pointer' }}
-                  editable={false}
-                  extensions={[sql(), EditorView.lineWrapping]}
-                  hidden={false}
-                  width='100%'
-                  height='100%'
-                  basicSetup={{
-                    lineNumbers: false
-                  }}
-                />
-              </ListItem>
-            ))}
-          </List>
-        </div>
-      ) : (
-        <div className='queries-list'>
-          <List>
-            {history.map((query, index) => (
-              <ListItem
-                key={index}
-                onClick={() => handleSelect(query)}
-              >
-                <CodeMirror
-                  style={{ cursor: 'pointer' }}
-                  value={query}
-                  editable={false}
-                  extensions={[sql(), EditorView.lineWrapping]}
-                  hidden={false}
-                  basicSetup={{
-                    lineNumbers: false
-                  }}
-                />
-              </ListItem>
-            ))}
-          </List>
-        </div>
-      )}
+
+      <TextField
+        size='small'
+        fullWidth
+        variant='outlined'
+        placeholder='Search queries...'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        style={{ margin: '8px 0' }}
+      />
+
+      <div className='queries-list'>
+        <List>
+          {filteredQueries.map((query, index) => (
+            <ListItem key={index} onClick={() => handleSelect(query)}>
+              <CodeMirror
+                value={query}
+                style={{ cursor: 'pointer' }}
+                editable={false}
+                extensions={[sql(), EditorView.lineWrapping]}
+                hidden={false}
+                width='100%'
+                height='100%'
+                basicSetup={{ lineNumbers: false }}
+              />
+            </ListItem>
+          ))}
+        </List>
+      </div>
     </div>
   );
 }
